422132d0388c4e13954862871ca64837d45dacbe
MOONJAEHOON <jh951229@naver.com>
2021-04-14 22:57:37 +0900

    CT 0414

---
+++ Programmers/문자열압축/문자열압축.py
@@ -0,0 +1,16 @@
+def zzz(s,i):
+    new_s, reduce,length =  '', 1, len(s)
+    while s:
+        if s[:i] == s[i:2*i]:
+            reduce += 1
+        else:
+            if reduce==1:
+                new_s += s[:i]
+            else:
+                new_s += f'{reduce}'+s[:i]
+            reduce=1
+        s = s[i:]
+    return len(new_s)
+
+def solution(s):
+    return min([zzz(s,i) for i in range( 1, int(len(s)//2)+1 )])
